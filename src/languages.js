182852e2bd78a6d4b17d926081401faa1b8c6afe
zjwgank <zjwgank@163.com>
2019-07-27 17:11:00 +0800

    feat: add language && add parser

--- src/languages.js
+++ src/languages.js
@@ -1,3 +1,5 @@
 export default {
-    
+    extensions:[".sh"],
+    name:'Shell',
+    parsers:["shell"]
 }
--- src/parsers.js
+++ src/parsers.js
@@ -1,3 +1,14 @@
+import parse from 'bash-parser'
+
 export default {
-    
+    shell:{
+        astFormat:'sh-ast',
+        parse:(text,parsers,options) => {
+            console.log(text,'====text')
+            console.log(parsers,'=====parsers')
+            console.log(options,'=====options')
+            console.log(text,parse(text),'=============')
+            return parse(text)
+        }
+    }
 }
